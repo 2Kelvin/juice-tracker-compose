c32b422d088ae82050b66e65fb99954cafe39ed9
2Kelvin <rocketmancodes02@gmail.com>
2024-08-16 13:08:36 +0300

    done

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,19 +1,3 @@
-/*
- * Copyright (C) 2023 The Android Open Source Project
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
@@ -72,6 +56,10 @@ dependencies {
     implementation("androidx.room:room-ktx:${rootProject.extra["room_version"]}")
     implementation("androidx.room:room-runtime:${rootProject.extra["room_version"]}")
     implementation("com.google.android.material:material:1.11.0")
+
+    // google play services ads dependency
+    implementation("com.google.android.gms:play-services-ads:22.2.0")
+
     ksp("androidx.room:room-compiler:${rootProject.extra["room_version"]}")
     debugImplementation("androidx.compose.ui:ui-tooling")
 }
